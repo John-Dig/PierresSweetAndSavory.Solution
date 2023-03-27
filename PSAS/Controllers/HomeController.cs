3ff17c25cb85a8a2291be39a38f8047bfeea60de
John Diggins <rohnfiggins@gmail.com>
2023-03-26 17:01:21 -0700

    Index good, working on AddFlavor

--- PSAS/Controllers/HomeController.cs
+++ PSAS/Controllers/HomeController.cs
@@ -8,35 +8,31 @@ using System.Security.Claims;
 
 namespace PSAS.Controllers
 {
-    public class HomeController : Controller
-    {
-      private readonly PSASContext _db;
-      private readonly UserManager<ApplicationUser> _userManager;
+  public class HomeController : Controller
+  {
+    private readonly PSASContext _db;
+    private readonly UserManager<ApplicationUser> _userManager;
 
-      public HomeController(UserManager<ApplicationUser> userManager, PSASContext db)
-      {
-        _userManager = userManager;
-        _db = db;
-      }
-  [HttpGet("/")]
-      public ActionResult Index()
-      {
-        return View();
-      }
-      // [HttpGet("/")]
-      // public async Task<ActionResult> Index()
-      // {
-     
-      //   string userId = this.User.FindFirst(ClaimTypes.NameIdentifier)?.Value;
-      //   ApplicationUser currentUser = await _userManager.FindByIdAsync(userId);
-      //   if (currentUser != null)
-      //   {
-      //   Treat[] treats = _db.Treats
-      //               .Where(entry => entry.User.Id == currentUser.Id)
-      //               .ToArray();
-      //   model.Add("treats", treats);
-      //   }
-      //   return View(model);
-      // }
+    public HomeController(UserManager<ApplicationUser> userManager, PSASContext db)
+    {
+      _userManager = userManager;
+      _db = db;
     }
+
+    [HttpGet("/")]
+    public async Task<ActionResult> Index()
+    {
+      Dictionary<string, object[]> model = new Dictionary<string, object[]>();
+      string userId = this.User.FindFirst(ClaimTypes.NameIdentifier)?.Value;
+      ApplicationUser currentUser = await _userManager.FindByIdAsync(userId);
+      if (currentUser != null)
+      {
+        Treat[] treats = _db.Treats
+                    .Where(entry => entry.User.Id == currentUser.Id)
+                    .ToArray();
+        model.Add("treats", treats);
+      }
+      return View(model);
+    }
+  }
 }
